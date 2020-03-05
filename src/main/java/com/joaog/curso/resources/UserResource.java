d434ddb04ea424a3cc492fd9e2f3074c8697ba63
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-03-05 12:58:29 -0300

    Entidade User e seu Resource

---
+++ src/main/java/com/joaog/curso/resources/UserResource.java
@@ -0,0 +1,21 @@
+package com.joaog.curso.resources;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.joaog.curso.entities.User;
+
+@RestController
+@RequestMapping(value = "/users") /* Caminho do recurso */
+public class UserResource {
+	
+	/* @GetMapping é uma notação para mapeamento de solicitações HTTP GET em métodos manipuladores específicos
+	 * ou seja, retorna o o caminho especificado no @RequestMapping */
+	@GetMapping
+	public ResponseEntity<User> findAll() {
+		User us = new User(1L, "joao", "joaogcm_jb@outlook.com", "17996175794", "joao123");
+		return ResponseEntity.ok().body(us);
+	}
+}

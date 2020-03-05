b0cef7f61892e82a64408063b129624241ba3aa2
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-03-05 15:13:44 -0300

    Camada de Serviço, Registro de Componentes

--- src/main/java/com/joaog/curso/resources/UserResource.java
+++ src/main/java/com/joaog/curso/resources/UserResource.java
@@ -1,22 +1,36 @@
 package com.joaog.curso.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.joaog.curso.entities.User;
+import com.joaog.curso.services.UserService;
 
 /* @ResController diz que a classe UserResource é um recurso web implementada por um controlador REST */
 @RestController
 @RequestMapping(value = "/users") /* Caminho do recurso */
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
 	/* @GetMapping é uma notação para mapeamento de solicitações HTTP GET em métodos manipuladores específicos
 	 * ou seja, retorna o o caminho especificado no @RequestMapping */
 	@GetMapping
-	public ResponseEntity<User> findAll() {
-		User us = new User(1L, "joao", "joaogcm_jb@outlook.com", "17996175794", "joao123");
-		return ResponseEntity.ok().body(us);
+	public ResponseEntity<List<User>> findAll() {
+		
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 }
---
+++ src/main/java/com/joaog/curso/services/UserService.java
@@ -0,0 +1,29 @@
+package com.joaog.curso.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.joaog.curso.entities.User;
+import com.joaog.curso.repositories.UserRepository;
+
+/* Registra a classe como Serviço do Spring */
+@Service
+public class UserService {
+	
+	/* Injeção de dependência */
+	@Autowired
+	private UserRepository repository;
+	
+	/* Retorna todos os usuários */
+	public List<User> findAll() {
+		return repository.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> obj = repository.findById(id);
+		return obj.get();
+	}
+}

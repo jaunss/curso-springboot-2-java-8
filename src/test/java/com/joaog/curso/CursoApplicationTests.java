5a89742410e0b15bbe4047a3cec947d329ef5a43
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-03-05 12:32:29 -0300

    Criação do Projeto

---
+++ src/test/java/com/joaog/curso/CursoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.joaog.curso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

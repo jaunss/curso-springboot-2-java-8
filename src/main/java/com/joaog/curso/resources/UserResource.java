4ef5ad37fa60a53f10301c292e790fd5368d206c
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-03-06 15:00:29 -0300

    Inserção de User

--- src/main/java/com/joaog/curso/resources/UserResource.java
+++ src/main/java/com/joaog/curso/resources/UserResource.java
@@ -1,13 +1,17 @@
 package com.joaog.curso.resources;
 
+import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.joaog.curso.entities.User;
 import com.joaog.curso.services.UserService;
@@ -16,21 +20,34 @@ import com.joaog.curso.services.UserService;
 @RestController
 @RequestMapping(value = "/users") /* Caminho do recurso */
 public class UserResource {
-	
+
 	@Autowired
 	private UserService service;
-	/* @GetMapping é uma notação para mapeamento de solicitações HTTP GET em métodos manipuladores específicos
-	 * ou seja, retorna o o caminho especificado no @RequestMapping */
+
+	/*
+	 * @GetMapping é uma notação para mapeamento de solicitações HTTP GET em métodos
+	 * manipuladores específicos ou seja, retorna o o caminho especificado
+	 * no @RequestMapping
+	 */
 	@GetMapping
 	public ResponseEntity<List<User>> findAll() {
-		
+
 		List<User> list = service.findAll();
 		return ResponseEntity.ok().body(list);
 	}
-	
+
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<User> findById(@PathVariable Long id){
+	public ResponseEntity<User> findById(@PathVariable Long id) {
 		User obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	/* Forma adequada de se inserir um recurso no banco de dados (Retorno 201 - Created) */
+	@PostMapping
+	public ResponseEntity<User> insert(@RequestBody User obj) {
+		obj = service.insert(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+				.path("/{id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).body(obj);
 	}
 }

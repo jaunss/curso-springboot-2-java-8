19bf8bbcfe330c739618a7de1875667d602b5007
Joao Gabriel Maciel <joaogabriel36@hotmail.com>
2020-03-06 11:27:37 -0300

    Associação Muitos para Muitos com JoinTable

--- src/main/java/com/joaog/curso/entities/Product.java
+++ src/main/java/com/joaog/curso/entities/Product.java
@@ -8,6 +8,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -23,9 +26,13 @@ public class Product implements Serializable {
 	private String name, description, imgUrl;
 	private Double price;
 	
-	/* Outro tipo de Lista */
 	/* @Transient impede que o JPA tente interpretar essa coleção de Lista */
-	@Transient
+	/* Outro tipo de Lista */
+	
+	@ManyToMany
+	@JoinTable(name = "tb_product_category",
+	joinColumns = @JoinColumn(name = "product_id"),
+	inverseJoinColumns = @JoinColumn(name = "category_id"))
 	private Set<Category> categories = new HashSet<Category>();
 	
 	public Product() {
